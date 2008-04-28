93af2405f76361c452a5307da1ecd5137d8c4105
hankin.robin <hankin.robin@gmail.com>
2008-04-28 00:00:00 +0000

    version 1.4-5

--- data/paulino.R
+++
@@ -1,8 +0,0 @@
-require("hyperdirichlet" , quietly=TRUE, save=FALSE)
-
-"paulino" <-
-  hyperdirichlet(
-                 x       = c(0,21,18,18,15,0,28,0) ,
-                 NC      = 8.6554760591751e-34 ,
-                 pnames  = c("low" , "medium" , "high")
-                 )
